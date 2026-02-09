f7d1c6700488a2b8f4b0607e679b3e3ca876c2fe
Shoxruh Tuxtanazarov <stuk@MacBook-Air-Shoxruh.local>
2026-02-09 15:42:52 +0500

    edit

--- src/app/router/AppRouter.jsx
+++ src/app/router/AppRouter.jsx
@@ -1,6 +1,5 @@
-// router/AppRouter.jsx
-import { Routes, Route } from 'react-router-dom';
-import { Suspense, lazy } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Suspense } from 'react';
 import MainLayout from '../layout/MainLayout';
 import RoleGuard from './RoleGuard';
 import { ROUTES } from './routes.config';
@@ -8,25 +7,25 @@ import Loading from '../../Components/Other/UI/Loadings/Loading';
 import NotFound from '../../Components/Common/404';
 import Login from '../../Components/Common/Login';
 
-
 export default function AppRouter() {
     return (
         <Suspense fallback={<Loading />}>
             <Routes>
+
+                {/* редирект с / на /login */}
+                <Route path="/" element={<Navigate to="/login" replace />} />
+
                 <Route element={<MainLayout />}>
                     {ROUTES.map(r => (
-                        <Route
-                            key={r.path}
-                            element={<RoleGuard allow={r.roles} />}
-                        >
+                        <Route key={r.path} element={<RoleGuard allow={r.roles} />}>
                             <Route path={r.path} element={<r.component />} />
                         </Route>
                     ))}
-
-                    {/* 🔥 404 — всегда в конце */}
                 </Route>
-                    <Route path="*" element={<NotFound />} />
-                    <Route path="/login" element={<Login />} />
+
+                <Route path="/login" element={<Login />} />
+                <Route path="*" element={<NotFound />} />
+
             </Routes>
         </Suspense>
     );
